0dce5a117d22ceda4c6c754739743ec5fb3b65ab
daniel crespo <hipiticliver@gmail.com>
2021-03-23 22:07:21 +0100

    Add files via upload
    Primer commit

---
+++ Deposito/src/main/java/cuentas/CCuenta.java
@@ -0,0 +1,137 @@
+package cuentas;
+
+/**
+ *
+ * @author Daniel Andrés Crespo de Torres
+ */
+
+public class CCuenta {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the tipoInterés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterés the tipoInterés to set
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+    /**
+     * Atributo nombre de tipo string
+     */
+    private String nombre;
+    /**
+     *atributo cuenta de tipo string
+     */
+    private String cuenta;
+    /**
+     * atributo saldo de tipo double
+     */
+    private double saldo;
+    /**
+     * atributo tipointeres de tipo double
+     */
+    private double tipoInterés;
+
+    /**
+     * constructor vacio
+     */
+    public CCuenta() {
+    }
+
+    /**
+     * constructor que inicializa las variables 
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo 
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
+    }
+
+    public double estado() {
+        return getSaldo();
+    }
+
+    /**
+     * Este método suma la cantidad a ingresar al saldo que teniamos y lanza una
+     * excepcion si la cantidad es menor a cero
+     *
+     * @param cantidad
+     * @throws Exception
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        }
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    /**
+     * Este metodo resta una cantidad al saldo y lanza una excepcion si la
+     * cantidad es menor o igual a cero y si el saldo que tenemos es menor que
+     * la cantidad que queremos quitar
+     *
+     * @param cantidad
+     * @throws Exception
+     */
+
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
+        setSaldo(getSaldo() - cantidad);
+    }
+}
